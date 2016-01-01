fee2168a0f9e86ab74ddd01076fd8fda903ed923
Johan Byttner <johan.byttner@gmail.com>
2016-01-01 23:31:54 +0000

    Added (new == 0) protection, so that calls can be chained in ft_lstadd

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,14 +6,21 @@
 /*   By: jbyttner <jbyttner@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 19:36:59 by jbyttner          #+#    #+#             */
-/*   Updated: 2015/11/27 19:38:59 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/01/01 23:30:51 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+** Added (new == 0) protection (so calls can be chained)
+*/
+
 void	ft_lstadd(t_list **alst, t_list *new)
 {
-	new->next = (*alst);
-	*alst = new;
+	if (*new)
+	{
+		new->next = (*alst);
+		*alst = new;
+	}
 }
