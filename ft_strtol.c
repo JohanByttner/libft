4ed512dacc89757b0f3cf36fcbd8e4759cd07fdc
Johan BYTTNER <jbyttner@student.42.fr>
2016-02-04 20:01:09 +0100

    Normed ft_strtol.c

--- ft_strtol.c
+++ ft_strtol.c
@@ -6,7 +6,7 @@
 /*   By: jbyttner <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/04 16:39:10 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/02/04 18:08:10 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/02/04 20:00:45 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,74 +14,90 @@
 #define FT_LONG_MAX 9223372036854775807L
 #define FT_LONG_MIN (-FT_LONG_MAX - 1)
 
-static inline void	ft_strtol_trim(const char *restrict s, int *base, int *sign)
+static inline const char	*ft_strtol_trim(const char *restrict s,
+		int *base, int *sign)
 {
 	char			c;
 
-	while (ft_isspace(*s)) 
+	while (ft_isspace(*s))
 		s++;
 	if (*s == '+' || *s == '-')
-		*sign = *s++ == '-' ? 1 : 0;
+		*sign = (*s++ == '-' ? 1 : 0);
 	c = *s++;
 	if ((*base == 0 || *base == 16) && c == '0' && ft_tolower(*s) == 'x')
 	{
 		*base = 16;
-		s++;
+		s += 2;
 	}
 	else if ((*base == 0 || *base == 2) && c == '0' && ft_tolower(*s) == 'b')
 	{
 		*base = 2;
-		s++;
+		s += 2;
 	}
 	if (*base == 0)
 		*base = c == '0' ? 8 : 10;
+	return (--s);
 }
 
-static inline const char	*ft_strtol_gen(const char *restrict s, unsigned long *acc,
-						int base, int sign)
+static inline int			ft_strtol_gen_c(char *c)
+{
+	if (ft_isdigit(*c))
+		*c -= '0';
+	else if (ft_isalpha(*c))
+		*c = 10 + (ft_tolower(*c) - 'a');
+	else
+		return (0);
+	return (1);
+}
+
+static inline const char	*ft_strtol_gen(const char *restrict s,
+		unsigned long *acc, int base, int *overflow)
 {
 	unsigned long	cutoff;
 	int				remnant;
 	char			c;
+	int				sign;
 
+	sign = *acc;
+	*acc = 0;
 	cutoff = sign ? -(unsigned long)FT_LONG_MIN : (unsigned long)FT_LONG_MAX;
 	remnant = cutoff % base;
 	cutoff /= base;
 	while ((c = *s++))
 	{
-		if (ft_isdigit(c))
-			c -= '0';
-		else if (ft_isalpha(c))
-			c = 10 + (ft_tolower(c) - 'a');
-		else
-			break ;
-		if (c >= base)
+		if (!(ft_strtol_gen_c(&c)) || c >= base)
 			break ;
 		if (*acc > cutoff || (*acc == cutoff && c > remnant))
 		{
-			*acc = sign ? FT_LONG_MIN : FT_LONG_MAX;
+			*overflow = -1;
 			break ;
 		}
+		*overflow = 1;
 		*acc *= base;
 		*acc += c;
 	}
 	return (s);
 }
 
-long			ft_strtol(const char *restrict s, char **restrict endp, int base)
+long						ft_strtol(const char *restrict s,
+		char **restrict endp, int base)
 {
 	int				sign;
-	int				b;
+	const char		*str;
+	int				overflow;
 	unsigned long	acc;
 
-	b = base;
+	str = s;
 	sign = 0;
-	acc = 0;
-	if (!(s))
-		return (0);
-	ft_strtol_trim(s, &b, &sign);
-	s = ft_strtol_gen(s, &acc,  b, sign);
+	overflow = 0;
+	str = ft_strtol_trim(str, &base, &sign);
+	acc = sign;
+	str = ft_strtol_gen(str, &acc, base, &overflow);
+	if (overflow == -1)
+		acc = sign ? FT_LONG_MIN : FT_LONG_MAX;
+	else if (sign)
+		acc = -acc;
 	if (endp)
-		*endp = (char *)s - 1;
+		*endp = (char *)(overflow ? str - 1 : s);
 	return (acc);
 }
