0896abbcd251b431c484cba6cb4f56852a2cc718
Johan <jbyttner@42.fr>
2016-05-06 03:01:09 +0100

    Fixing alignment warning with gcc

--- srcs/ft_memset.c
+++ srcs/ft_memset.c
@@ -6,13 +6,18 @@
 /*   By: jbyttner <jbyttner@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/25 15:34:10 by jbyttner          #+#    #+#             */
-/*   Updated: 2016/05/06 01:36:07 by jbyttner         ###   ########.fr       */
+/*   Updated: 2016/05/06 02:12:01 by jbyttner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <limits.h>
 #define MIN_WORDS 4
+#if UINT_MAX > 0xffffffff
+# define FT_MEMSET_SHIFT_32 32
+#else
+# define FT_MEMSET_SHIFT_32 0
+#endif
 
 void	*ft_memset(void *s, int c, size_t n)
 {
@@ -32,7 +37,7 @@ void	*ft_memset(void *s, int c, size_t n)
 			ch = (ch << 8) | ch;
 			if ((UINT_MAX > 0xffff && (ch = (ch << 16) | ch)) || 1)
 				if (UINT_MAX > 0xffffffff)
-					ch = (ch << 32) | ch;
+					ch = (ch << FT_MEMSET_SHIFT_32) | ch;
 		}
 		while (i * sizeof(unsigned int) < n)
 			((unsigned int *)arr)[i++] = ch;
